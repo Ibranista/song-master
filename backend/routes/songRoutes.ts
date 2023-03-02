f78006921c247b3c89a70098a5b283a3e2a0f1d7
Ibranista <ibranista88@gmail.com>
2023-03-02 08:28:22 +0100

    add route to list songs.

--- backend/routes/songRoutes.ts
+++ backend/routes/songRoutes.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { createSong } from "../controllers/songController";
+import { createSong, listSongs } from "../controllers/songController";
 
 const router = express.Router();
 
 router.post("/createSong", createSong);
+router.get("/", listSongs);
 export default router;
