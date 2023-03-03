36522736140a6ca57150f1bd79c06e5908c9d124
Ibranista <ibranista88@gmail.com>
2023-03-03 08:41:29 +0100

    generate total number of songs and albums each artist has.

--- client/src/components/TotalStatistics.tsx
+++ client/src/components/TotalStatistics.tsx
@@ -28,6 +28,43 @@ function TotalStatistics() {
       genres.push(genre);
     }
   }
+  let genreCount: any = {};
+  for (let i = 0; i < songs.length; i++) {
+    let genre = songs[i].genre.toLowerCase().trim();
+    if (genre in genreCount) {
+      genreCount[genre]++;
+    } else {
+      genreCount[genre] = 1;
+    }
+  }
+  const countArtist: any = {};
+  for (let i = 0; i < songs.length; i++) {
+    let artist = songs[i].artist.toLowerCase();
+    if (artist in countArtist) {
+      countArtist[artist].songs++;
+      if (!countArtist[artist].albums.includes(songs[i].album)) {
+        countArtist[artist].albums.push(songs[i].album);
+      }
+    } else {
+      countArtist[artist] = {
+        songs: 1,
+        albums: [songs[i].album],
+      };
+    }
+  }
+  const artistNumbers: any = [];
+  for (const [artist, count] of Object.entries(countArtist)) {
+    artistNumbers.push(
+      <div
+        key={artist}
+        style={{ border: "1px solid black", padding: "10px", margin: "10px" }}
+      >
+        <h1>Artist: {artist}</h1>
+        <h2>Total songs: {count.songs}</h2>
+        <h2>Total albums: {count.albums.length}</h2>
+      </div>
+    );
+  }
   return (
     <div>
       <h1>Total Statistics</h1>
@@ -39,6 +76,21 @@ function TotalStatistics() {
       </ul>
       <h1>Specific Stats</h1>
       <h1>total number of songs in every genre</h1>
+      <section>
+        {Object.keys(genreCount).map((genre) => {
+          return (
+            <div>
+              <h1>
+                {genre} :{genreCount[genre]}
+              </h1>
+            </div>
+          );
+        })}
+      </section>
+      <section>
+        <h1>Total of songs & albums each artist has</h1>
+        {artistNumbers}
+      </section>
     </div>
   );
 }
